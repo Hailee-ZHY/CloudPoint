6e4f82ccfa5c6e5a79d3f20f9456086bfaeb96d1
HaileeZ <haileehyz@gmail.com>
2025-04-06 11:56:57 -0700

    update

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Training process"
-python train.py --epoches 20 --batch_size 8 --lr 0.001
+python train.py --epoches 20 --batch_size 8 --lr 0.001 
 
 echo "Test Results"
 python main.py  
